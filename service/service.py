b08f0958a1de4aeeed0f3eba4f1890fce1661807
qijinze911 <qi@10-16-188-241.dynapool.wireless.nyu.edu>
2019-10-10 07:43:57 +0800

    Create service.py and complete list API

--- service/service.py
+++ service/service.py
@@ -12,12 +12,109 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-“”“
+"""
 Recommendation Service
 
 Paths:
 -----
+list: HTTP GET /recommendations
+read: HTTP GET /recommendations/{rec-id}
+query: HTTP GET /recommendations?product-id=123&type=upscale&customer-id=111
+create: HTTP POST /recommendations
+update: HTTP PUT /recommendations/{rec-id}
+delete: HTTP DELETE /recommendations/{rec-id}
+success: HTTP PUT /recommendations/{rec-id}/success
+"""
 
+import os
+import sys
+import logging
+from flask import Flask, jsonify, request, url_for, make_response, abort
+from flask_api import status    # HTTP Status Codes
+from werkzeug.exceptions import NotFound
 
+# For this example we'll use SQLAlchemy, a popular ORM that supports a
+# variety of backends including SQLite, MySQL, and PostgreSQL
+from flask_sqlalchemy import SQLAlchemy
+from service.models import Pet, DataValidationError
+
+# Import Flask application
+from . import app
+
+######################################################################
+# Error Handlers
+######################################################################
+@app.errorhandler(DataValidationError)
+def request_validation_error(error):
+    """ Handles Value Errors from bad data """
+    return bad_request(error)
+
+@app.errorhandler(status.HTTP_400_BAD_REQUEST)
+def bad_request(error):
+    """ Handles bad reuests with 400_BAD_REQUEST """
+    message = str(error)
+    app.logger.warning(message)
+    return jsonify(status=status.HTTP_400_BAD_REQUEST,
+                   error='Bad Request',
+                   message=message), status.HTTP_400_BAD_REQUEST
+
+@app.errorhandler(status.HTTP_404_NOT_FOUND)
+def not_found(error):
+    """ Handles resources not found with 404_NOT_FOUND """
+    message = str(error)
+    app.logger.warning(message)
+    return jsonify(status=status.HTTP_404_NOT_FOUND,
+                   error='Not Found',
+                   message=message), status.HTTP_404_NOT_FOUND
+
+@app.errorhandler(status.HTTP_405_METHOD_NOT_ALLOWED)
+def method_not_supported(error):
+    """ Handles unsuppoted HTTP methods with 405_METHOD_NOT_SUPPORTED """
+    message = str(error)
+    app.logger.warning(message)
+    return jsonify(status=status.HTTP_405_METHOD_NOT_ALLOWED,
+                   error='Method not Allowed',
+                   message=message), status.HTTP_405_METHOD_NOT_ALLOWED
+
+@app.errorhandler(status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
+def mediatype_not_supported(error):
+    """ Handles unsuppoted media requests with 415_UNSUPPORTED_MEDIA_TYPE """
+    message = str(error)
+    app.logger.warning(message)
+    return jsonify(status=status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
+                   error='Unsupported media type',
+                   message=message), status.HTTP_415_UNSUPPORTED_MEDIA_TYPE
+
+@app.errorhandler(status.HTTP_500_INTERNAL_SERVER_ERROR)
+def internal_server_error(error):
+    """ Handles unexpected server error with 500_SERVER_ERROR """
+    message = str(error)
+    app.logger.error(message)
+    return jsonify(status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                   error='Internal Server Error',
+                   message=message), status.HTTP_500_INTERNAL_SERVER_ERROR
+                   
+######################################################################
+# LIST ALL RECOMMENDATIONS
+######################################################################
+@app.route('/recommendations', methods=['HTTP GET'])
+def list_recommendations():
+    """ Returns all of the Recommendations """
+    app.logger.info('Request for recommendation list')
+    recommendations = []
+    product_id = request.args.get('product_id')
+    customer_id = request.args.get('customer_id')
+    recommend_type = request.args.get('recommend_type')
+    if product_id:
+        recommendations = Recommendation.find_by_product_id(product_id)
+    elif customer_id:
+        recommendations = Recommendation.find_by_customer_id(customer_id)
+    elif recommend_type:
+        recommendations = Recommendation.find_by_recommend_type(recomend_type)
+    else:
+        recommendations = Recommendation.all()
+
+    results = [recomendation.serialize() for recommendation in recommendations]
+    return make_response(jsonify(results), status.HTTP_200_OK)
 
 
