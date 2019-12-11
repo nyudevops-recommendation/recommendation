f4450151f1ef287c2f3729fd4afa0ddbb976c4fe
qijinze911 <157124834@qq.com>
2019-12-10 22:21:27 -0500

    Update_apidocs

--- service/service.py
+++ service/service.py
@@ -155,7 +155,7 @@ class RecommendationResource(Resource):
     @api.response(204, 'Recommendation deleted')
     def delete(self, rec_id):
         """
-        Delete a recommendation
+        Delete a Recommendation
     
         This endpoint will delete a recommendation based the id specified in the path
         """
@@ -221,6 +221,7 @@ class ApplyResource(Resource):
     def put(self, rec_id):
         """
         Increment A Recommendation's Success Field
+        This endpoint will increment the success counter based on the recommendtion succeed
         """
         app.logger.info('Increment success field for recommendation with id: %s', rec_id)
         check_content_type('application/json')
